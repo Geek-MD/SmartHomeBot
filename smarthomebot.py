bd729a266b26d0628a7e43b009db5bd25a016830
Edison Montes M <edison.montes@gmail.com>
2022-03-22 23:25:57 -0300

    Update smarthomebot.py

--- smarthomebot.py
+++ smarthomebot.py
@@ -16,14 +16,17 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, Callb
 # bot variables
 AUTH_TOKEN = "bot_token"
 ALLOWED_USERS = ["user_id_1", "user_id_2"]
-commands = ['/start', '/help']
+ADMIN_USERS = ["user_id_1"]
+commands = ['/start', '/help', '/reboot']
+admin_commands = ['/reboot']
 
 # multiline markup text used for /help command.
 help_command_text = """This is a simple Telegram Bot used to automate notifications for a Smart Home\.
 
 *Available commands*
 \/start \- does nothing, bot starts automatically
-\/help \- shows a list of all available commands"""
+\/help \- shows a list of all available commands
+\/reboot \- reboots system \(restricted to admins\)"""
 # end of multiline text
 
 # enable logging.
@@ -39,11 +42,17 @@ def start_command(update: Update, context: CallbackContext) -> None:
 def help_command(update: Update, context: CallbackContext) -> None:
     update.message.reply_markdown_v2(help_command_text)
 
+def reboot_command(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text('Sorry, reboot command is dissabled for now.')
+
 def not_command(update: Update, context: CallbackContext) -> None:
     update.message.reply_text('Sorry, I can\'t understand that.')
 
 def not_allowed_users(update: Update, context: CallbackContext) -> None:
     update.message.reply_text('Sorry you\'re not allowed to use this bot')
+
+def not_admin(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text('Sorry, you\'re not an admin, you can\'t use admin restricted commands.')
 
 def main() -> None:
     # create the Updater and pass it your bot's token.
@@ -55,12 +64,16 @@ def main() -> None:
    # not allowed users can't interact with the bot.
     updater.dispatcher.add_handler(MessageHandler(Filters.user(ALLOWED_USERS), not_allowed_users))
 
+   # not admin users can't run admin restricted commands.
+    dispatcher.add_handler(MessageHandler(Filters.text(admin_commands) & ~Filters.user(ADMIN_USERS), not_admin))
+
     # commands.
     dispatcher.add_handler(CommandHandler("start", start_command))
     dispatcher.add_handler(CommandHandler("help", help_command))
+    dispatcher.add_handler(CommandHandler("reboot", reboot_command))
 
     # on non command i.e message, reply with not_command function.
-    updater.dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.text(commands), not_command))
+    updater.dispatcher.add_handler(MessageHandler(Filters.text & Filters.text(commands), not_command))
 
     # start the bot.
     updater.start_polling()
