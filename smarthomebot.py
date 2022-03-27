db25023fc50967d24b938cf8c7c3d0563f85ff9c
Edison Montes M <edison.montes@gmail.com>
2022-03-27 19:52:59 -0300

    Update smarthomebot.py

--- smarthomebot.py
+++ smarthomebot.py
@@ -9,16 +9,19 @@ Usage:
 Use /help to list available commands.
 """
 
-import logging
-from telegram import Update, ForceReply, User
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+import logging, os, time
+from telegram import Update, User, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext, CallbackQueryHandler
 
 # bot variables
-AUTH_TOKEN = "bot_token" # string
+AUTH_TOKEN = "bot-token" # string
 ALLOWED_USERS = [user_id_1, user_id_2] # integer
-ADMIN_USERS = [user_id_1] # integer
+ADMIN_USERS = [user_id_1] #integer
 commands = ['/start', '/help', '/reboot']
 admin_commands = ['/reboot']
+reboot_keyboard = [[InlineKeyboardButton("yes", callback_data='y'), InlineKeyboardButton("no", callback_data='n')]]
+reboot_keyboard_markup = InlineKeyboardMarkup(reboot_keyboard)
+reboot_option = None
 
 # multiline markup text used for /help command.
 help_command_text = """This is a simple Telegram Bot used to automate notifications for a Smart Home\.
@@ -43,7 +46,28 @@ def help_command(update: Update, context: CallbackContext) -> None:
     update.message.reply_markdown_v2(help_command_text)
 
 def reboot_command(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text('Sorry, reboot command is dissabled for now.')
+    global reboot_option
+    button_pressed = False
+    update.message.reply_text('Reboot your system?', reply_markup=reboot_keyboard_markup)
+
+def reboot_query(update: Update, context: CallbackContext) -> None:
+    global reboot_option
+    button_pressed = False
+    query = update.callback_query
+    query.answer()
+    reboot_option = query.data
+    while button_pressed == False:
+        if reboot_option == "y":
+            query.edit_message_text(text=f'Rebooting in 5 secs...')
+            time.sleep(5)
+            os.system("sudo reboot")
+            break
+        elif reboot_option == "n":
+            query.edit_message_text(text=f'Reboot aborted.')
+            button_pressed = True
+            break
+        elif reboot_option == None:
+            continue
 
 def not_command(update: Update, context: CallbackContext) -> None:
     update.message.reply_text('Sorry, I can\'t understand that.')    
@@ -61,6 +85,9 @@ def main() -> None:
 
     # not allowed users can't interact with the bot.
     updater.dispatcher.add_handler(MessageHandler(~Filters.user(ALLOWED_USERS), not_allowed_users))
+
+    # inline buttons.
+    updater.dispatcher.add_handler(CallbackQueryHandler(reboot_query))
 
     # on non command i.e message, reply with not_command function.
     updater.dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.text(commands), not_command))
